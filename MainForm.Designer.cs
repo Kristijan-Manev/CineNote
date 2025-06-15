7d40c68640eb4a051bdcb7c6feb1196696ee8fef
Kristijan-Manev <108988476+Kristijan-Manev@users.noreply.github.com>
2025-06-16 01:20:07 +0200

    Renamed MainForm, created folder structure, added layout dashboard

---
+++ MainForm.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace CineNote
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelSidebar = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.btnAddMovie = new System.Windows.Forms.Button();
+            this.btnWatchlist = new System.Windows.Forms.Button();
+            this.btnStats = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panelSidebar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSidebar
+            // 
+            this.panelSidebar.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panelSidebar.Controls.Add(this.btnExit);
+            this.panelSidebar.Controls.Add(this.btnStats);
+            this.panelSidebar.Controls.Add(this.btnWatchlist);
+            this.panelSidebar.Controls.Add(this.btnAddMovie);
+            this.panelSidebar.Controls.Add(this.btnHome);
+            this.panelSidebar.Controls.Add(this.label1);
+            this.panelSidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSidebar.ForeColor = System.Drawing.Color.DimGray;
+            this.panelSidebar.Location = new System.Drawing.Point(0, 0);
+            this.panelSidebar.Name = "panelSidebar";
+            this.panelSidebar.Size = new System.Drawing.Size(200, 450);
+            this.panelSidebar.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 403);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CineNote";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackColor = System.Drawing.Color.Transparent;
+            this.btnHome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.ForeColor = System.Drawing.Color.White;
+            this.btnHome.Location = new System.Drawing.Point(0, 0);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(200, 34);
+            this.btnHome.TabIndex = 2;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = false;
+            this.btnHome.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelContent
+            // 
+            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContent.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.panelContent.Location = new System.Drawing.Point(200, 0);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(600, 450);
+            this.panelContent.TabIndex = 1;
+            // 
+            // btnAddMovie
+            // 
+            this.btnAddMovie.BackColor = System.Drawing.Color.Transparent;
+            this.btnAddMovie.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAddMovie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddMovie.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddMovie.ForeColor = System.Drawing.Color.White;
+            this.btnAddMovie.Location = new System.Drawing.Point(0, 34);
+            this.btnAddMovie.Name = "btnAddMovie";
+            this.btnAddMovie.Size = new System.Drawing.Size(200, 34);
+            this.btnAddMovie.TabIndex = 3;
+            this.btnAddMovie.Text = "Add Movie";
+            this.btnAddMovie.UseVisualStyleBackColor = false;
+            // 
+            // btnWatchlist
+            // 
+            this.btnWatchlist.BackColor = System.Drawing.Color.Transparent;
+            this.btnWatchlist.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnWatchlist.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWatchlist.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWatchlist.ForeColor = System.Drawing.Color.White;
+            this.btnWatchlist.Location = new System.Drawing.Point(0, 68);
+            this.btnWatchlist.Name = "btnWatchlist";
+            this.btnWatchlist.Size = new System.Drawing.Size(200, 34);
+            this.btnWatchlist.TabIndex = 4;
+            this.btnWatchlist.Text = "Watchlist";
+            this.btnWatchlist.UseVisualStyleBackColor = false;
+            // 
+            // btnStats
+            // 
+            this.btnStats.BackColor = System.Drawing.Color.Transparent;
+            this.btnStats.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnStats.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStats.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStats.ForeColor = System.Drawing.Color.White;
+            this.btnStats.Location = new System.Drawing.Point(0, 102);
+            this.btnStats.Name = "btnStats";
+            this.btnStats.Size = new System.Drawing.Size(200, 33);
+            this.btnStats.TabIndex = 5;
+            this.btnStats.Text = "Stats";
+            this.btnStats.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(0, 135);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(200, 34);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelContent);
+            this.Controls.Add(this.panelSidebar);
+            this.Name = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panelSidebar.ResumeLayout(false);
+            this.panelSidebar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSidebar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Panel panelContent;
+        private System.Windows.Forms.Button btnAddMovie;
+        private System.Windows.Forms.Button btnWatchlist;
+        private System.Windows.Forms.Button btnStats;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
